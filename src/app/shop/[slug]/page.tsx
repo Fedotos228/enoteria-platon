ddca642dcc550738a6e77911d000595652646df9
Orbu Petru <porbu@sapec.mdporbu@sapec.mdporbu@sapec.md>
2024-06-18 10:35:42 +0300

    fix(ui): label build error

--- src/app/shop/[slug]/page.tsx
+++ src/app/shop/[slug]/page.tsx
@@ -4,26 +4,30 @@ import Container from '@/components/layout/Container'
 import { productsService } from '@/services/products/products.service'
 
 export type ProductSiglePageProps = {
-  params: {
-    slug: string
-  }
+	params: {
+		slug: string
+	}
 }
 
 export async function generateStaticParams() {
-  const { data } = await productsService.getProductsByFetch()
+	const { data } = await productsService.getProductsByFetch()
 
-  return data.map((product: any) => ({
-    slug: product.attributes.slug,
-  }))
+	return data.map((product: any) => ({
+		slug: product.attributes.slug,
+	}))
 }
 
 export default function ProductSiglePage({ params }: ProductSiglePageProps) {
-  const title = params.slug.replace(/-/g, ' ').split(' ').map((word: string) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+	const title = params.slug
+		.replace(/-/g, ' ')
+		.split(' ')
+		.map((word: string) => word.charAt(0).toUpperCase() + word.slice(1))
+		.join(' ')
 
-  return (
-    <Container>
-      <Banner title={title} />
-      <ProductSingle slug={params.slug} />
-    </Container>
-  )
+	return (
+		<Container>
+			<Banner title={title} />
+			<ProductSingle slug={params.slug} />
+		</Container>
+	)
 }
