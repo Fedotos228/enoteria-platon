f41503e3203ef394dae57a2809b5c3cc57a8a205
Orbu Petru <porbu@sapec.mdporbu@sapec.mdporbu@sapec.md>
2024-06-26 10:58:43 +0300

    feat(ui): mobile filters

--- src/components/elements/navigation/Navigation.tsx
+++ src/components/elements/navigation/Navigation.tsx
@@ -17,49 +17,6 @@ export default function Navigation({
 }: INavigationProps) {
   const pathname = usePathname();
 
-<<<<<<< HEAD
-	return (
-		<>
-			{screenWidth < 767 && (
-				<div
-					onClick={() => setIsOpen(false)}
-					className={`fixed inset-0 backdrop-blur-md transition-all duration-300 bg-opacity-75 md:hidden ${isOpen ? 'opacity-100 visible' : 'opacity-0 invisible'
-						}`}
-				></div>
-			)}
-			<nav
-				className={`flex justify-start md:justify-center transition-all duration-300 gap-6 ml:gap-8 fixed inset-y-0 md:inset-0 w-full bg-background flex-col pl-10 pt-14 md:p-0 md:items-center items-start md:flex-row md:bg-transparent h-full max-w-lg md:relative ${isOpen ? 'right-0' : '-right-full'
-					}`}
-			>
-				{screenWidth < 767 && (
-					<Button
-						className='absolute top-4 right-4 md:hidden'
-						onClick={() => setIsOpen(false)}
-						variant='ghost'
-						size='icon'
-					>
-						<X size={24} />
-					</Button>
-				)}
-				{NAVIGATION_ITEMS.map(
-					(item, i) =>
-						item.href && (
-							<Link
-								href={item.href}
-								key={i}
-								className={`text-white hover:text-bordo-hover transition-colors duration-300 md:text-base text-lg md:text-background text-foreground ${pathname === item.href
-										? 'font-semibold !text-bordo'
-										: ''
-									}`}
-							>
-								{item.title}
-							</Link>
-						),
-				)}
-			</nav>
-		</>
-	)
-=======
   return (
     <>
       {screenWidth < 767 && (
@@ -87,13 +44,13 @@ export default function Navigation({
         )}
         {NAVIGATION_ITEMS.map(
           (item, i) =>
-            item.url && (
+            item.href && (
               <Link
-                href={item.url}
+                href={item.href}
                 onClick={() => setIsOpen(false)}
                 key={i}
                 className={`text-lg text-foreground transition-colors duration-300 hover:text-bordo-hover md:text-base md:text-background md:text-white ${
-                  pathname === item.url ? "font-semibold !text-bordo" : ""
+                  pathname === item.href ? "font-semibold !text-bordo" : ""
                 }`}
               >
                 {item.title}
@@ -103,5 +60,4 @@ export default function Navigation({
       </nav>
     </>
   );
->>>>>>> fdb488d3becc77d94b8fba8fad7a8e0b174a295a
 }
