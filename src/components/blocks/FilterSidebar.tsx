2df03ca112f2b951f47a3b0b6031e9d9cfbd0799
Orbu Petru <porbu@sapec.mdporbu@sapec.mdporbu@sapec.md>
2024-06-26 11:14:03 +0300

    fix(ui): fix shop grid & filters

--- src/components/blocks/FilterSidebar.tsx
+++ src/components/blocks/FilterSidebar.tsx
@@ -61,61 +61,64 @@ export default function FilterSidebar() {
       </div>
     </div>
   ) : (
-    <Sheet>
-      <SheetTrigger asChild>
-        <Button variant="outline" className="gap-3">
-          <SlidersHorizontal size={20} />
-          Filtre
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="flex flex-col">
-        <div className="flex-1">
-          {category?.map((categorie: any) => (
-            <div key={categorie.attributes.slug} className="mb-7">
-              <h5 className="mb-3">{categorie.attributes.title}</h5>
-
-              <ul>
-                {categorie?.attributes?.subcategories.data.map(
-                  (subcategory: any) => (
-                    <li
-                      key={subcategory.attributes.slug}
-                      className="mb-3 flex items-center space-x-2"
-                    >
-                      <Checkbox
-                        id={subcategory.attributes.slug}
-                        className="border border-[#FFFFFF]/80 bg-[#E1D7D3]"
-                        onClick={() =>
-                          toggleCategory(subcategory.attributes.slug)
-                        }
-                      />
-                      <label
-                        htmlFor={subcategory.attributes.slug}
-                        className={`${
-                          categorie.attributes.slug === "coloare"
-                            ? `circle ${subcategory.attributes.slug}`
-                            : ""
-                        } text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70`}
-                      >
-                        {subcategory.attributes.title}
-                      </label>
-                    </li>
-                  ),
-                )}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <SheetFooter className="sticky bottom-0 flex-row gap-2">
-          <Button className="flex-1" onClick={() => refetch()}>
-            <TextSearch size={20} />
-            Caută
+    <>
+      <span></span>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button variant="outline" className="gap-3 md:hidden">
+            <SlidersHorizontal size={20} />
+            Filtre
           </Button>
-          {/* <Button variant="outline" size="icon" onClick={clearFilters}>
+        </SheetTrigger>
+        <SheetContent className="flex flex-col">
+          <div className="flex-1">
+            {category?.map((categorie: any) => (
+              <div key={categorie.attributes.slug} className="mb-7">
+                <h5 className="mb-3">{categorie.attributes.title}</h5>
+
+                <ul>
+                  {categorie?.attributes?.subcategories.data.map(
+                    (subcategory: any) => (
+                      <li
+                        key={subcategory.attributes.slug}
+                        className="mb-3 flex items-center space-x-2"
+                      >
+                        <Checkbox
+                          id={subcategory.attributes.slug}
+                          className="border border-[#FFFFFF]/80 bg-[#E1D7D3]"
+                          onClick={() =>
+                            toggleCategory(subcategory.attributes.slug)
+                          }
+                        />
+                        <label
+                          htmlFor={subcategory.attributes.slug}
+                          className={`${
+                            categorie.attributes.slug === "coloare"
+                              ? `circle ${subcategory.attributes.slug}`
+                              : ""
+                          } text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70`}
+                        >
+                          {subcategory.attributes.title}
+                        </label>
+                      </li>
+                    ),
+                  )}
+                </ul>
+              </div>
+            ))}
+          </div>
+          <SheetFooter className="sticky bottom-0 flex-row gap-2">
+            <Button className="flex-1" onClick={() => refetch()}>
+              <TextSearch size={20} />
+              Caută
+            </Button>
+            {/* <Button variant="outline" size="icon" onClick={clearFilters}>
             <ListRestart size={20} />
           </Button> */}
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </>
   );
 }
 
