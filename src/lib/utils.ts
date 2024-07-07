61fe63ed1b38fe06516e0c9f7ed7c596d89373ed
Fedotos228 <tudor.onceanu@gmail.com>
2024-07-07 15:12:22 +0300

    zaebali cu imaginile

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,5 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
 export enum MediaType {
   Single,
@@ -7,60 +7,61 @@ export enum MediaType {
 }
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
-export const isClient = typeof window !== "undefined";
+export const isClient = typeof window !== "undefined"
 
 export function imageStrapUrl(image: any, type: MediaType) {
-  if (!image) return "";
+  if (!image) return ""
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'production') {
     switch (type) {
       case MediaType.Single:
-        return image?.data?.attributes?.url;
+        return image?.data?.attributes?.url
       case MediaType.Multiple:
-        return image?.attributes?.url;
+        return image?.attributes?.url
     }
-  } else {
+  }
+  if (process.env.NODE_ENV === 'development') {
     switch (type) {
       case MediaType.Single:
-        return process.env.NEXT_PUBLIC_BASE_URL + image?.data?.attributes?.url;
+        return process.env.NEXT_PUBLIC_BASE_URL + image?.data?.attributes?.url
       case MediaType.Multiple:
-        return process.env.NEXT_PUBLIC_BASE_URL + image?.attributes?.url;
+        return process.env.NEXT_PUBLIC_BASE_URL + image?.attributes?.url
     }
   }
 }
 export const formatRONPrice = (price: number) => {
-  if (!price) return null;
+  if (!price) return null
   return price.toLocaleString("ro-RO", {
     style: "currency",
     currency: "RON",
     minimumFractionDigits: 0,
-  });
-};
+  })
+}
 
 export const formatMDLPrice = (price: number) => {
-  if (!price) return null;
+  if (!price) return null
   return price.toLocaleString("ro-RO", {
     style: "currency",
     currency: "MDL",
     minimumFractionDigits: 0,
-  });
-};
+  })
+}
 
 export function generateYears(start: number, end: number) {
-  const years = [];
+  const years = []
   for (let year = end; year >= start; year--) {
-    years.push(year.toString());
+    years.push(year.toString())
   }
-  return years;
+  return years
 }
 
 export function generateMonths() {
-  const months = [];
+  const months = []
   for (let month = 1; month <= 12; month++) {
-    months.push(month.toString().padStart(2, "0"));
+    months.push(month.toString().padStart(2, "0"))
   }
-  return months;
+  return months
 }
