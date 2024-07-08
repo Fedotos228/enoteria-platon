55ba9d479d6b4fee2eca37f59593ecb92499e418
cornelber <bercornel@gmail.com>
2024-07-08 16:09:10 +0300

    modified CheckoutForm & CheckoutFormSchema, removed PaymentForm.tsx (from Checkout) & AddressForm.tsx as separate files

--- src/components/forms/CheckoutForm.tsx
+++ src/components/forms/CheckoutForm.tsx
@@ -1,29 +1,42 @@
-"use client"
+"use client";
 
-import { useActions } from '@/hooks/useActions'
-import { useAppSelector } from "@/store/store"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useEffect } from 'react'
-import { useForm } from "react-hook-form"
-import { toast } from "sonner"
+import { useActions } from "@/hooks/useActions";
+import { useAppSelector } from "@/store/store";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { useSelector } from "react-redux";
+import useCreateOrder from "@/hooks/mutations/useCreateOrder";
 
-import { Form } from "@/components/ui/form"
-import useCreateOrder from '@/hooks/mutations/useCreateOrder'
-import { Button } from "../ui/button"
-import { AddressForm } from "./blocks/AddressForm"
-import { PaymentForm } from "./blocks/PaymentForm"
 import {
   CheckoutFormSchema,
   CheckoutFormSchemaType,
-} from "./schemas/CheckoutFormSchema"
-import { useSelector } from 'react-redux'
-import { selectCartTotal } from '@/store/slices/cart.slice'
+} from "./schemas/CheckoutFormSchema";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "../ui/button";
+import { selectCartTotal } from "@/store/slices/cart.slice";
+import { RadioGroup, RadioGroupItem } from "@radix-ui/react-radio-group";
+import { Label } from "@radix-ui/react-label";
 
 export function CheckoutForm() {
-  const { mutate: createOrder, isPending, isSuccess, isError } = useCreateOrder()
-  const { products, shipping } = useAppSelector((state) => state.cart)
-  const total = useSelector(selectCartTotal)
-  const { clearCart } = useActions()
+  const {
+    mutate: createOrder,
+    isPending,
+    isSuccess,
+    isError,
+  } = useCreateOrder();
+  const { products, shipping } = useAppSelector((state) => state.cart);
+  const total = useSelector(selectCartTotal);
+  const { clearCart } = useActions();
 
   const defaultValues: CheckoutFormSchemaType = {
     firstName: "",
@@ -34,45 +47,39 @@ export function CheckoutForm() {
     city: "",
     country: "moldova",
     paymentMethod: "card",
-    cardNumber: "",
-    cardName: "",
-    expiryMonth: "",
-    expiryYear: "",
-    cvv: "",
     shipping: 0,
     subTotalPrice: 0,
     totalPrice: 0,
-  }
+  };
 
   const form = useForm<CheckoutFormSchemaType>({
     resolver: zodResolver(CheckoutFormSchema),
     defaultValues,
-  })
-
+  });
 
   useEffect(() => {
-    const totalPrice = total + shipping
-    form.setValue('totalPrice', totalPrice)
-    form.setValue('shipping', shipping)
-    form.setValue('subTotalPrice', total)
-  }, [form, total, shipping])
+    const totalPrice = total + shipping;
+    form.setValue("totalPrice", totalPrice);
+    form.setValue("shipping", shipping);
+    form.setValue("subTotalPrice", total);
+  }, [form, total, shipping]);
 
   function onSubmit(data: CheckoutFormSchemaType) {
     if (products.length === 0) {
       toast.error("Nu există produse în coșul de cumpărături!", {
         position: "top-center",
-      })
+      });
+      return; // Oprirea funcției onSubmit dacă nu există produse în coș
     }
     // Add logic to send data here
-    let order = { ...data, products }
-
-    createOrder(order)
+    let order = { ...data, products };
+    console.log(order);
+    createOrder(order);
 
     if (isSuccess) {
-      clearCart()
-      form.reset(defaultValues)
+      clearCart();
+      form.reset(defaultValues);
     }
-
   }
 
   return (
@@ -81,15 +88,218 @@ export function CheckoutForm() {
         className="grid w-full grid-cols-1 py-4"
         onSubmit={form.handleSubmit(onSubmit)}
       >
-        <AddressForm control={form.control} />
-        <PaymentForm control={form.control} />
+        <div className="mb-8">
+          <div className="col-span-2 mb-8">
+            <h2 className="text-lg font-bold uppercase text-bordo">
+              Adresa Poștala
+            </h2>
+          </div>
+          <div className="grid grid-cols-2 gap-5">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Numele <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="h-12"
+                      placeholder="ex. Popescu"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Prenumele <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input className="h-12" placeholder="ex. Ion" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Email <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="h-12"
+                      inputMode="email"
+                      type="email"
+                      placeholder="ex. example@example.com"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="phone"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Telefon <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="h-12"
+                      inputMode="tel"
+                      type="tel"
+                      placeholder="ex. +37367525214"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="address"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Adresă <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="h-12"
+                      placeholder="ex. Strada Nr., Bloc, Apartament"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="city"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Oraș <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      className="h-12"
+                      placeholder="ex. Chișinău"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="country"
+              render={({ field }) => (
+                <FormItem className="col-span-2">
+                  <FormLabel>Țară</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      className="flex gap-3"
+                      value={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <div className="flex items-center">
+                        <RadioGroupItem
+                          value="moldova"
+                          id="Moldova"
+                          className="me-1 h-4 w-4 rounded-full border border-black/70 bg-origin-content transition-all data-[state=checked]:border-[white] data-[state=checked]:bg-[#3B3640]"
+                        />
+                        <Label htmlFor="Moldova" className="cursor-pointer">
+                          Moldova
+                        </Label>
+                        <RadioGroupItem
+                          value="romania"
+                          id="Romania"
+                          className="me-1 ms-8 h-4 w-4 rounded-full border border-black/70 ring-offset-2 ring-offset-black transition-all data-[state=checked]:border-[white] data-[state=checked]:bg-[#3B3640]"
+                        />
+                        <Label htmlFor="Romania" className="cursor-pointer">
+                          Romania
+                        </Label>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="paymentMethod"
+              render={({ field }) => (
+                <FormItem className="col-span-2">
+                  <FormLabel>Modalitatea de plata</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      className="flex gap-3"
+                      value={field.value}
+                      onValueChange={field.onChange}
+                    >
+                      <div className="flex flex-col sm:flex-row">
+                        <div className="flex items-center">
+                          <RadioGroupItem
+                            value="card"
+                            id="card"
+                            className="me-1 h-4 w-4 rounded-full border border-black/70 bg-origin-content transition-all data-[state=checked]:border-[white] data-[state=checked]:bg-[#3B3640]"
+                          />
+                          <Label htmlFor="card" className="cursor-pointer">
+                            Card de credit/debit
+                          </Label>
+                        </div>
+                        <div className="flex items-center">
+                          <RadioGroupItem
+                            value="paypal"
+                            id="paypal"
+                            className="me-1 h-4 w-4 rounded-full border border-black/70 bg-origin-content transition-all data-[state=checked]:border-[white] data-[state=checked]:bg-[#3B3640] sm:ms-8"
+                          />
+                          <Label htmlFor="paypal" className="cursor-pointer">
+                            Paypal
+                          </Label>
+                        </div>
+                        <div className="flex items-center">
+                          <RadioGroupItem
+                            value="cash"
+                            id="cash"
+                            className="me-1 h-4 w-4 rounded-full border border-black/70 bg-origin-content transition-all data-[state=checked]:border-[white] data-[state=checked]:bg-[#3B3640] sm:ms-8"
+                          />
+                          <Label htmlFor="cash" className="cursor-pointer">
+                            Cash
+                          </Label>
+                        </div>
+                      </div>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </div>
         <Button className="ml-auto inline-block px-6" type="submit" size="sm">
-          {
-            isPending ? 'Se procesează...' : 'Achită'
-          }
-
+          {isPending ? "Se procesează..." : "Achită"}
         </Button>
       </form>
     </Form>
-  )
+  );
 }
--- src/components/forms/schemas/CheckoutFormSchema.tsx
+++ src/components/forms/schemas/CheckoutFormSchema.tsx
@@ -1,6 +1,6 @@
-import { z } from "zod"
+import { z } from "zod";
 
-export const AddressFormSchema = z.object({
+const AddressFormSchema = z.object({
   firstName: z
     .string()
     .min(3, { message: "Numele trebuie să aibă cel puțin 3 caractere" }),
@@ -18,60 +18,14 @@ export const AddressFormSchema = z.object({
     .string()
     .min(2, { message: "Orașul trebuie să aibă cel puțin 2 caractere" }),
   country: z.enum(["moldova", "romania"]),
-})
+});
 
-export const CardPaypalPaymentSchema = z.object({
-  cardNumber: z.string().min(16, {
-    message: "Numărul cardului trebuie să aibă cel puțin 16 cifre",
-  }),
-  cardName: z.string().min(4, {
-    message: "Numele și prenumele trebuie să aibă cel puțin 4 caractere",
-  }),
-  expiryMonth: z
-    .string()
-    .min(1, { message: "Luna de expirare este obligatorie" }),
-  expiryYear: z
-    .string()
-    .min(1, { message: "Anul de expirare este obligatoriu" }),
-  cvv: z
-    .string()
-    .min(3, { message: "CVV-ul trebuie să aibă cel puțin 3 caractere" }),
-})
+export const CheckoutFormSchema = z.object({
+  ...AddressFormSchema.shape,
+  paymentMethod: z.enum(["card", "paypal", "cash"]),
+  shipping: z.number(),
+  subTotalPrice: z.number(),
+  totalPrice: z.number(),
+});
 
-export const CashPaymentSchema = z.object({
-  cardNumber: z.string().optional(),
-  cardName: z.string().optional(),
-  expiryMonth: z.string().optional(),
-  expiryYear: z.string().optional(),
-  cvv: z.string().optional(),
-})
-
-// export const PaymentFormSchema = z.object({
-//   paymentMethod: z.enum(["card", "paypal", "cash"]),
-//   ...CashPaymentSchema.shape, // default Schema
-// });
-
-export const CheckoutFormSchema = z
-  .object({
-    ...AddressFormSchema.shape,
-    paymentMethod: z.enum(["card", "paypal", "cash"]),
-    shipping: z.number(),
-    subTotalPrice: z.number(),
-    totalPrice: z.number(),
-  })
-  .and(
-    z.union([
-      z.object({
-        paymentMethod: z.literal("cash"),
-        ...CashPaymentSchema.shape,
-      }),
-      z.object({
-        paymentMethod: z.enum(["card", "paypal"]),
-        ...CardPaypalPaymentSchema.shape,
-      }),
-    ]),
-  )
-
-// export type PaymentFormSchemaType = z.infer<typeof PaymentFormSchema>;
-// export type AddressFormSchemaType = z.infer<typeof AddressFormSchema>;
-export type CheckoutFormSchemaType = z.infer<typeof CheckoutFormSchema>
+export type CheckoutFormSchemaType = z.infer<typeof CheckoutFormSchema>;
