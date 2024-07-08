57e3cd27fea676d6f9f619f9e291984c7137d3fe
cornelber <bercornel@gmail.com>
2024-07-08 16:19:40 +0300

    removed unnecessary functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -40,22 +40,6 @@ export const formatMDLPrice = (price: number) => {
   });
 };
 
-export function generateYears(start: number, end: number) {
-  const years = [];
-  for (let year = end; year >= start; year--) {
-    years.push(year.toString());
-  }
-  return years;
-}
-
-export function generateMonths() {
-  const months = [];
-  for (let month = 1; month <= 12; month++) {
-    months.push(month.toString().padStart(2, "0"));
-  }
-  return months;
-}
-
 export function dateFormater(data: string) {
   const date = new Date(data);
 
