244c38be244909337e04458d8c300b30f923dfa4
Fedotos228 <tudor.onceanu@gmail.com>
2024-07-09 16:08:54 +0300

    pulled Cornel's mods

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,29 +1,27 @@
-import { useRouter } from "next/router";
-import { Onest } from "next/font/google";
-import { Toaster } from "@/components/ui/sonner";
-import "../globals.css";
+import { Toaster } from "@/components/ui/sonner"
+import { Onest } from "next/font/google"
+import "../globals.css"
 
-const onest = Onest({ subsets: ["latin"] });
-
+const onest = Onest({ subsets: ["latin"] })
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
 
   return (
     <html lang="en">
       <body className={`${onest.className} overflow-x-hidden`}>
         <div className="toaster">
-            <Toaster richColors position="top-center" theme="light" />
+          <Toaster richColors position="top-center" theme="light" />
         </div>
         <div className="flex min-h-screen flex-col">
-            <main className="flex-1">
-                {children}
-            </main>
+          <main className="flex-1">
+            {children}
+          </main>
         </div>
       </body>
     </html>
-  );
+  )
 }
