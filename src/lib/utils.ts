1e5d007c2c72cf5db76a16e1ab38b8fdbe65802b
Fedotos228 <tudor.onceanu@gmail.com>
2024-07-09 15:18:40 +0300

    formater

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -46,5 +46,5 @@ export function dateFormater(data: string) {
   const year = date.getUTCFullYear()
   const month = ("0" + (date.getUTCMonth() + 1)).slice(-2)
   const day = ("0" + date.getUTCDate()).slice(-2)
-  return `${month}.${day}.${year}`
+  return `${day}.${month}.${year}`
 }
