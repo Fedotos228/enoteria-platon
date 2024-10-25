c050dd73b4ba7dca5955d490870c71a6d161615f
Fedotos228 <tudor.onceanu@gmail.com>
2024-10-25 11:45:16 +0300

    asdfd

--- src/app/[locale]/(admin)/orders/page.tsx
+++ src/app/[locale]/(admin)/orders/page.tsx
@@ -2,14 +2,16 @@
 
 import { OrdersTable } from "@/components/cards/OrdersTable"
 import { getToken } from "@/lib/localStorage"
+import { useLocale } from 'next-intl'
 import { useRouter } from "next/navigation"
 
 export default function OrdersPage() {
+  const locale = useLocale()
   const token = getToken()
   const { replace } = useRouter()
 
   if (!token) {
-    replace("/orders/auth")
+    replace(`${locale}/orders/auth`)
   } else {
     return <OrdersTable />
   }
